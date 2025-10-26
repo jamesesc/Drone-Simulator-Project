914a2891339336a11451799196f9c730798bb527
OisinPG <oisinpgilbert@gmail.com>
2025-10-26 15:43:08 -0700

    Added methods to AnomalyDetector for GPS spoofing

--- src/AnomalyDetector.java
+++ src/AnomalyDetector.java
@@ -1,6 +1,9 @@
 import Model.Drone;
 import Model.TelemetryData;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class AnomalyDetector {
     /**
      * What constitutes a battery being too low
@@ -27,7 +30,97 @@ public class AnomalyDetector {
      * Assumed to be degrees and not radians.
      */
     final double UPSIDE_DOWN = 360.0;
+    /**
+     * The upper and lower bounds for latitude and longitude.
+     * Lower bound latitude, lower bound longitude, upper bound latitude, upper bound longitude.
+     */
+    final double[] OUT_OF_BOUNDS = {-1000.0, -1000.0, 1000.0, 1000.0};
 
+    record Location(double lat, double lon) {}
+
+    /**
+     * Detects any teleportation of a Drone. This method treats the drone as if
+     * it's on a 2D grid, with longitude being x and latitude being y.
+     *
+     * @param thePrior The prior state of the drone.
+     * @param theCurrent The current state of the drone.
+     * @param theTimeStep Time difference between thePrior's data and theCurrent's data.
+     * @return Whether there was teleportation.
+     */
+    public boolean detectTeleport(TelemetryData thePrior, TelemetryData theCurrent, double theTimeStep) {
+        //Angles to radians
+        double prior_angle_radians = Math.toRadians(thePrior.getOrientation());
+        //delta x = v * cos(theta) * t, delta y = v * sin(theta) * t
+        double dX = thePrior.getVelocity() * Math.cos(prior_angle_radians) * theTimeStep;
+        double dY = thePrior.getVelocity() * Math.sin(prior_angle_radians) * theTimeStep;
+        //Actual displacement
+        double aDX = theCurrent.getLongitude() - thePrior.getLongitude();
+        double aDY = theCurrent.getLatitude() - thePrior.getLatitude();
+        //Pythagoras type deal
+        double expected_distance = Math.sqrt(Math.pow(dX, 2)
+                + Math.pow(dY, 2));
+        double actual_distance = Math.sqrt(Math.pow(aDX, 2)
+                + Math.pow(aDY, 2));
+
+        //MARGIN OF ERROR, percentage of theCurrent expected needs to pass
+        double marginOfError = 1.5;
+        //Avoiding false positives from zero movement
+        double minThreshold = 0.01;
+
+        double allowedDistance = Math.max(expected_distance * marginOfError, minThreshold);
+
+        return actual_distance > allowedDistance;
+    }
+
+    /**
+     * Whether any drone shares the same longitude and latitude (drones can't be inside each other).
+     *
+     * @param theDrones The drones we are checking.
+     * @return Whether a drone is inside another.
+     */
+    public boolean sharingLocations(Drone[] theDrones) {
+        boolean result = false;
+
+        Set<Location> seen = new HashSet<>();
+
+        for (Drone drone : theDrones) {
+            Location loc = new Location(
+                    drone.getMyDroneTelemetryData().getLatitude(),
+                    drone.getMyDroneTelemetryData().getLongitude()
+            );
+
+            if (seen.contains(loc)) {
+                result = true;
+            }
+            seen.add(loc);
+        }
+        return result;
+    }
+
+
+    /**
+     * Whether a drone is outside the given bounds.
+     *
+     * @param theState The state of the drone we're checking.
+     * @return Whether the drone is out of bounds.
+     */
+    public boolean outOfBounds(TelemetryData theState) {
+        boolean result = false;
+
+        Location loc = new Location(
+                theState.getLatitude(),
+                theState.getLongitude()
+        );
+
+        if (loc.lon < OUT_OF_BOUNDS[1] || loc.lon > OUT_OF_BOUNDS[3]) {
+            result = true;
+        }
+        if (loc.lat < OUT_OF_BOUNDS[0] || loc.lat > OUT_OF_BOUNDS[2]) {
+            result = true;
+        }
+
+        return result;
+    }
     /**
      * Using a drone's current data, determines whether a drone is flying backwards.
      *
