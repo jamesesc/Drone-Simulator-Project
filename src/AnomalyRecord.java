7423300b00834d25402312dcf73b1d5cefb4a1c5
OisinPG <oisinpgilbert@gmail.com>
2025-10-27 14:51:23 -0700

    Added substance to AnomalyRecord.java Added 3 different analyzeDrones methods to AnomalyDetector (each are different based on given parameters).

--- src/AnomalyRecord.java
+++ src/AnomalyRecord.java
@@ -1,2 +1,57 @@
 public class AnomalyRecord {
+    /**
+     * Name of method you had an error in.
+     */
+    private String myMethod;
+    /**
+     * ID of the drone w/ an error, null if it's multiple drones.
+     */
+    private int myID;
+    /**
+     * Time that the error happened.
+     */
+    private double myTime;
+
+    //Constructors
+    public AnomalyRecord() {
+        myMethod = "";
+        myTime = 0;
+    }
+
+    public AnomalyRecord(String theMethod, int theID, double theTime) {
+        myMethod = theMethod;
+        myID = theID;
+        myTime = theTime;
+    }
+
+    public AnomalyRecord(String theMethod, double theTime) {
+        myMethod = theMethod;
+        myTime = theTime;
+    }
+
+    // Getters
+    public String getMethod() {
+        return myMethod;
+    }
+
+    public int getID() {
+        return myID;
+    }
+
+    public double getTime() {
+        return myTime;
+    }
+
+    // Setters
+    public void setMethod(String theMethod) {
+        myMethod = theMethod;
+    }
+
+    public void setID(int theID) {
+        myID = theID;
+    }
+
+    public void setTime(double theTime) {
+        myTime = theTime;
+    }
 }
