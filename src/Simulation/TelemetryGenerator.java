9c03384ec3450f5fcd3b2606f73505aa968b12e8
James <jamesl.escudero@gmail.com>
2025-10-30 00:01:22 -0700

    feat: implemented velocity generator base on weighted chances

--- src/Simulation/TelemetryGenerator.java
+++ src/Simulation/TelemetryGenerator.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 public class TelemetryGenerator {
 
-    private final Random randomNumGenerator = new Random();
+    private static final Random randomNumGenerator = new Random();
 
     public TelemetryData generateTelemetryData() {
         final TelemetryData telemetryData = new TelemetryData();
@@ -45,7 +45,64 @@ public class TelemetryGenerator {
     }
 
     private void generateVelocity(final TelemetryData theTelemetryData) {
-        final double randomVelocity = randomNumGenerator.nextDouble() * 100;
-        theTelemetryData.setVelocity(randomVelocity);
+
+
+        /* Velocity: 0-100 m/s
+        60-100 m/s: High
+        30-60 m/s: Average
+        10-30 m/s: Average
+        0-10 m/s: Low
+        */
+
+        int[][] velocityRange = {
+                {0,10},
+                {11, 30},
+                {31, 60},
+                {61, 101}
+        };
+
+        int[] velocityRangeWeight = {15, 45, 35, 5};
+
+
+        theTelemetryData.setVelocity(velocityPicker(velocityRange, velocityRangeWeight));
+    }
+
+    private static double velocityPicker(final int[][] theVelocityRange, final int[] theVelocityRangeWeight) {
+        int totalWeightSum = 0;
+        for (int weight : theVelocityRangeWeight) {
+            totalWeightSum += weight;
+        }
+
+        int velocityRangePicker = randomNumGenerator.nextInt(totalWeightSum);
+
+        int velocityRangeIndex = 0;
+        int totalSum = 0;
+
+        for (int i = 0; i < theVelocityRangeWeight.length; i++) {
+            totalSum += theVelocityRangeWeight[i];
+            if (velocityRangePicker < totalSum) {
+                velocityRangeIndex = i;
+                break;
+            } else {
+                totalSum += theVelocityRangeWeight[i];
+            }
+        }
+
+        // random.nextInt(upperBound - lowerBound) + lowerBound;
+
+        int upperBound = theVelocityRange[velocityRangeIndex][1];
+        int lowerBound = theVelocityRange[velocityRangeIndex][0];
+
+        return Math.round(lowerBound + (upperBound - lowerBound) * randomNumGenerator.nextDouble());
+    }
+
+    public static void main(String[] args) {
+        TelemetryData telemetryDataTest = new TelemetryData();
+        TelemetryGenerator telemetryGenTest = new TelemetryGenerator();
+        for (int i = 0; i < 100; i++) {
+            telemetryGenTest.generateVelocity(telemetryDataTest);
+            System.out.println(telemetryDataTest.getVelocity());
+        }
+
     }
 }
