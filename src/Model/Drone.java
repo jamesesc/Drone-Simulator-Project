3334af9a5e37a446ee2b244f7c3e262dbae8b1ed
James <jamesl.escudero@gmail.com>
2025-10-29 21:08:21 -0700

    doc: added javadoc to the drone class

--- src/Model/Drone.java
+++ src/Model/Drone.java
@@ -1,59 +1,96 @@
 package Model;
 
+/**
+ * A class that represents the drone that we're simulating on.
+ * Uses another object called Telemetry Data which acts as the storage
+ * for the drone Telemetry data.
+ *
+ * @author James Escudero
+ * @version Fall 2025
+ */
 public class Drone {
+
+    /* FIEldS */
+
+    /** A telemetry data object class used to store all the drone Telemetry data */
     private TelemetryData myDroneTelemetryData = new TelemetryData();
 
-    private int myBattery;
-
+    /** An int that represents the drone ID */
     private static int myDroneID;
 
+    /** An int that represents the Battery level the drone has */
+    private int myBattery;
+
+    /** A boolean that represent whether the drone is on or no */
     private boolean myIsDroneOn;
 
+    /** Constant that represent the amount of power the Battery */
     private static final int BATTERY_DECREASE = 10;
 
+
+    /* CONSTRUCTORS */
+
+    /** A non-arg constructor that initializes the drone id, and set the drone status to on */
     public Drone() {
-        myDroneTelemetryData = null;
         myDroneID += 1;
         myIsDroneOn = true;
-
     }
 
+    /**
+     * An arg constructor that takes a telemetry data and sets that telemetry data to the drone
+     *
+     * @param droneTelemetryData represents the wanted telemetry data to assign to the drone.
+     */
     public Drone(TelemetryData droneTelemetryData) {
         myDroneTelemetryData = droneTelemetryData;
         myDroneID += 1;
         myIsDroneOn = true;
     }
 
+
     /* GETTERS */
 
+    /** A getter to get the drone battery level */
     public int getBatteryLevel() {
         return myBattery;
     }
 
+    /** A getter to get the drone ID */
     public int getDroneID() {
         return myDroneID;
+    }
+
+    /** A getter to get the drone Telemetry Data */
+    public TelemetryData getDroneTelemetryData() {
+        return myDroneTelemetryData;
+    }
+
+    /** A getter to get whether the drone is on or not */
+    public boolean isDroneOn() {
+        return myIsDroneOn;
     }
 
 
     /* METHODS */
 
+    /** A setter to set the Drone Battery Level.
+     *
+     * @param theNewBatteryLevel represents the new drone battery level.
+     */
     public void setBatteryLevel(final int theNewBatteryLevel) {
         myBattery = theNewBatteryLevel;
     }
 
+    /** A method to handle in decrementing the battery */
     protected void decrementBattery() {
         myBattery -= BATTERY_DECREASE;
     }
 
-    public boolean isDroneOn() {
-        return myIsDroneOn;
-    }
-
+    /** A setter to update the drone Telemetry Data
+     *
+     * @param theNewTelemetryData represent the telemetry data that is going to update the drone telemetry data.
+     */
     public void updateTelemetryData(final TelemetryData theNewTelemetryData) {
         myDroneTelemetryData = theNewTelemetryData;
-    }
-
-    public TelemetryData getMyDroneTelemetryData() {
-        return myDroneTelemetryData;
     }
 }
