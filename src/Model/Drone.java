c79ef55061449612c1afa5f30ba4172cc0435e43
James <jamesl.escudero@gmail.com>
2025-11-07 12:24:34 -0800

    feat: implemented drone initalize battery generator

--- src/Model/Drone.java
+++ src/Model/Drone.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.util.Random;
+
 /**
  * A class that represents the drone that we're simulating on.
  * Uses another object called Telemetry Data which acts as the storage
@@ -29,6 +31,9 @@ public class Drone {
 
     /** Constant that represent the amount of power the Battery */
     private static final int BATTERY_DECREASE = 10;
+
+    /** Random generator to help generate random battery level */
+    Random randomNumGen = new Random();
 
 
     /* CONSTRUCTORS */
@@ -107,6 +112,54 @@ public class Drone {
 
     /* METHODS */
 
+    /** Method that randomly weight the drone battery level when first initalize */
+    private void initializeDroneSetting() {
+        // The range that battery can randomly generate
+        final int[][] batteryLevelRange = {
+                {0, 19}, // Low
+                {20, 49}, // Medium
+                {50, 100} // Full
+        };
+
+        // The probability for each range to occur (related to the above array)
+        final int[] probBatteryLevel = {1, 14, 85};
+
+        // Represent the total probability (should be 100)
+        int totalProb = 0;
+
+        // Loop through the probability array and add up the probability
+        for (int probabilityWeight : probBatteryLevel) {
+            totalProb += probabilityWeight;
+        }
+
+        // Choosing a random number between the total probability
+        int randomLevel = randomNumGen.nextInt(totalProb);
+        // Used to know which range we are in that we randomly generated
+        int selectedRangeIndex = 0;
+
+        /* Going through a probability array, subtracting the prob level until its below 0
+         and assign rangeIndex to the range we found*/
+        for (int i = 0; i < probBatteryLevel.length; i++) {
+            randomLevel -= probBatteryLevel[i];
+
+            if (randomLevel < 0) {
+                selectedRangeIndex = i;
+                break;
+            }
+        }
+
+        // Storing the range min and max
+        int min = batteryLevelRange[selectedRangeIndex][0];
+        int max = batteryLevelRange[selectedRangeIndex][1];
+
+        // Base on the right category, just choose a random num between those bounds
+        // +1 because random(19) only includes 0-18... so we need to be one higher
+        int batterLevel = min + randomNumGen.nextInt(max - min + 1); // Inclusive
+
+        // Setting the battery level to the new generated batter level
+        setBatteryLevel(batterLevel);
+    }
+
     /** Method to update the drone Batter */
     public void updateDroneBattery() {
         /* Velocity Power => Battery Consumption Chart
@@ -127,7 +180,6 @@ public class Drone {
         }
         myBattery -= batteryDecrease;
     }
-
 
     /* MAIN METHOD */
 
