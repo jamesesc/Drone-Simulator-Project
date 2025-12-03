bf44d04c231f7e78254f4c4f71bf66f1d1be2ff5
James <jamesl.escudero@gmail.com>
2025-12-03 01:05:23 -0800

    refactor: complete SimulationListener

--- src/view/SimulationListener.java
+++ src/view/SimulationListener.java
@@ -1,13 +1,47 @@
-package controller;
+package view;
 
 import Model.AnomalyRecord;
 import Model.Drone;
+import service.TimerManager;
 
+/**
+ * Interface for listening to simulation events.
+ *
+ * @version Autumn 2025
+ */
 public interface SimulationListener {
-
+    /**
+     * Called when the elapsed time is updated.
+     *
+     * @param theElapsedTime the elapsed time in milliseconds.
+     */
     void onTimeUpdate(int theElapsedTime);
 
+    /**
+     * Called when there is an update to the drone fleet.
+     *
+     * @param theFleet the updated array of drones.
+     */
     void onDroneUpdate(Drone[] theFleet);
 
+    /**
+     * Called when anomalies are detected.
+     *
+     * @param anomaly the array of detected anomalies.
+     */
     void onAnomaliesDetected(AnomalyRecord[] anomaly);
+
+    /**
+     * Called when the drone fleet is reloaded.
+     *
+     * @param theFleet the reloaded array of drones.
+     */
+    void onFleetReloaded(Drone[] theFleet);
+
+    /**
+     * Called when the status of the timer manager changes.
+     *
+     * @param theStatus the new status of the timer manager.
+     */
+    void onStatusChanged(TimerManager.Status theStatus);
 }
