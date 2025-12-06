1043ef92ab254044e49cdf99be02593fd0a113fe
James <jamesl.escudero@gmail.com>
2025-12-06 11:11:10 -0800

    refactor: implemented a private helper to initalize the backend

--- src/App/Simulation.java
+++ src/App/Simulation.java
@@ -16,11 +16,15 @@ import view.MonitorDash;
 /**
  * A program that simulates drones.
  *
- * @version Autumn 2025.
+ * @version Fall 2025.
  */
 public class Simulation extends Application {
 
-    //Start the application
+    /**
+     * Starts the simulation.
+     *
+     * @param theArgs represents the string of arguments.
+     */
     public static void main(final String[] theArgs) {
         launch(theArgs);
     }
@@ -29,17 +33,27 @@ public class Simulation extends Application {
     public void start(Stage stage) {
         // Creating all the classes
         TelemetryGenerator telemetryGen = new TelemetryGenerator();
-        TimerManager timerManager = new TimerManager();
-        AnomalyDB anomalyDB = new AnomalyDB();
-        AnomalyDetector anomalyDetector = new AnomalyDetector();
-        DroneFactory droneFactory = new DroneFactory();
-        DroneFleetManager fleetManager = new DroneFleetManager(telemetryGen, droneFactory);
-        SimulationEngine scheduler = new SimulationEngine(timerManager, fleetManager, anomalyDetector, anomalyDB);
-        DroneMonitorApp controller = new DroneMonitorApp(timerManager, scheduler, fleetManager, anomalyDB);
+        final DroneMonitorApp controller = createDroneMonitorApp(telemetryGen);
         MonitorDash monitorDash = new MonitorDash(controller);
         UpdateUIManager updateUIManager = new UpdateUIManager(monitorDash);
 
         controller.setSimulationListener(updateUIManager);
         monitorDash.initializeSimulation(stage);
+    }
+
+    /**
+     * Helper method to initialize all the backend components.
+     *
+     * @param theTelemetryGen represent the telemetry generator being used in the back end.
+     * @return a DroneMonitorApp object, which is the back end controller.
+     */
+    private static DroneMonitorApp createDroneMonitorApp(final TelemetryGenerator theTelemetryGen) {
+        TimerManager timerManager = new TimerManager();
+        AnomalyDB anomalyDB = new AnomalyDB();
+        AnomalyDetector anomalyDetector = new AnomalyDetector();
+        DroneFactory droneFactory = new DroneFactory();
+        DroneFleetManager fleetManager = new DroneFleetManager(theTelemetryGen, droneFactory);
+        SimulationEngine scheduler = new SimulationEngine(timerManager, fleetManager, anomalyDetector, anomalyDB);
+        return new DroneMonitorApp(timerManager, scheduler, fleetManager, anomalyDB);
     }
 }
