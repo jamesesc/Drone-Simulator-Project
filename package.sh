c90af2515f303588291c4fbea4536ae23db10dde
mankirat mann <mannmankirat@gmail.com>
2025-12-06 17:35:36 -0800

    Delete package.sh

--- package.sh
+++
@@ -1,9 +0,0 @@
-#/usr/bin/env bash
-rm -rf out/package/FileWatcher
-~/.jdks/openjdk-25.0.1-1/bin/jpackage.exe \
---type app-image  \
---app-version 0.1.0 --description "TCSS360 FileWatcher" \
---name "FileWatcher" --vendor "Group 5" \
---main-jar "./Drone Simulator Project.jar" \
---input ./out/artifacts/Drone_Simulator_Project_jar --main-class app.simulation \
---dest out/package
